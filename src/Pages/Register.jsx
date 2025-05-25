1dbde9ce655071c5c36e2478e144d523c095dd31
FahimX <sakibfahim270@gmail.com>
2025-05-26 02:11:06 +0800

    firebase auth setup

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,23 +1,54 @@
+import Lottie from "lottie-react";
 import React from "react";
+import registerLottie from "../assets/register.json";
 
 const Register = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const registerData = {
+      email,
+      password,
+    };
+    console.log(registerData);
+  };
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <div className="text-center lg:text-left"></div>
+        <div className="text-center lg:text-left">
+          <Lottie
+            style={{ width: "250px" }}
+            animationData={registerLottie}
+            loop={true}
+          ></Lottie>
+        </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <fieldset className="fieldset">
-              <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
-              <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
-              <div>
-                <a className="link link-hover">Forgot password?</a>
-              </div>
-              <button className="btn btn-neutral mt-4">Login</button>
-            </fieldset>
+            <h1 className="text-5xl font-bold">Register Now</h1>
+            <form onSubmit={handleRegister}>
+              <fieldset className="fieldset">
+                <label className="label">Email</label>
+                <input
+                  name="email"
+                  type="email"
+                  className="input"
+                  placeholder="Email"
+                />
+                <label className="label">Password</label>
+                <input
+                  name="password"
+                  type="password"
+                  className="input"
+                  placeholder="Password"
+                />
+                <div>
+                  <a className="link link-hover">Forgot password?</a>
+                </div>
+                <button className="btn btn-neutral mt-4">Register</button>
+              </fieldset>
+            </form>
           </div>
         </div>
       </div>
